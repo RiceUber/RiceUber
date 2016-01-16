b950c24ea5f8b00edc637e66ee74ec9dd84da154
Jack Petersen <jackpetersen@Jacks-MacBook-Pro.local>
2016-01-16 15:14:36 -0600

    HTML stuff

--- test.py
+++ test.py
@@ -1,24 +1,28 @@
-#import os
-#import urlparse
-#import psycopg2
-#from flask import Flask, render_template, request
-#import calendar
-#import time
-#import collections
-#import scipy.integrate
-#import logging
-#import datetime
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for,  abort, render_template, flash
 
-#import sqlite3
-#from flask import Flask, request, session, g, redirect, url_for,  abort, render_template, flash
 
-from flask import Flask
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
-#app.config.from_object(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template('homepage.html')
+    return render_template('homepage.html')
 
-	if __name__ == '__main__':
-		app.run()
+@app.route('/addride')
+def add_ride():
+	pass
+
+@app.route('/data')
+def data():
+	pass
+
+
+if __name__ == '__main__':
+    app.run()
