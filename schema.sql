32a05ce187e03c9f7e3920ae9db6fca58bb0adc9
Robyn <robyn.torregrosa@gmail.com>
2016-01-16 20:22:13 -0600

    tables

--- schema.sql
+++ schema.sql
@@ -1,6 +1,10 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
-  title text not null,
-  text text not null
+  name text not null,
+  email text not null,
+  phone text not null,
+  datetime text not null,
+  fromloc text not null,
+  toloc text not null
 );
